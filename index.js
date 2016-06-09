9499dad6f711b2a69813dbe4c22a80ce99807af8
John Lennard <john@yakmoo.se>
2016-06-10 00:25:17 +1200

    fundamental argument split

--- index.js
+++ index.js
@@ -15,6 +15,7 @@
         CONSTANTS = require('./src/constants'),
         calendar = require('./src/calendars'),
         astrometry = require('./src/astrometry'),
+        fundamentalArguments = require('./src/fundamental-arguments'),
         ephemerides = require('./src/ephemerides'),
         HH = require('./src/heap-helper'),
         ASTROM = require('./src/astrom'),
@@ -118,58 +119,14 @@
             return SH.chunkArray(Array.from(retMatrix), 3);
         };
 
-
-
     var writeFloat64Buffer = HH.writeFloat64Buffer,
         readFloat64Buffer = HH.readFloat64Buffer;
-
-
-
-
-
-
 
     var erfa = {
         _Module : LIBERFA,
         ASTROM : ASTROM,
         LDBODY: LDBODY,
         CONSTANTS: CONSTANTS,
-
-
-
-
-
-
-
-        //Fundamental arguments
-        /** double eraFal03(double t); */
-        fal03: LIBERFA.cwrap('eraFal03', 'number', ['number']),
-        /** double eraFalp03(double t); */
-        falp03: LIBERFA.cwrap('eraFalp03', 'number', ['number']),
-        /** double eraFaf03(double t); */
-        faf03: LIBERFA.cwrap('eraFaf03', 'number', ['number']),
-        /** double eraFad03(double t); */
-        fad03: LIBERFA.cwrap('eraFad03', 'number', ['number']),
-        /** double eraFaom03(double t); */
-        faom03: LIBERFA.cwrap('eraFaom03', 'number', ['number']),
-        /** double eraFave03(double t); */
-        fave03: LIBERFA.cwrap('eraFave03', 'number', ['number']),
-        /** double eraFae03(double t); */
-        fae03: LIBERFA.cwrap('eraFae03', 'number', ['number']),
-        /** double eraFapa03(double t); */
-        fapa03: LIBERFA.cwrap('eraFapa03', 'number', ['number']),
-        /** double eraFame03(double t); */
-        fame03: LIBERFA.cwrap('eraFame03', 'number', ['number']),
-        /** double eraFama03(double t); */
-        fama03: LIBERFA.cwrap('eraFama03', 'number', ['number']),
-        /** double eraFaju03(double t); */
-        faju03: LIBERFA.cwrap('eraFaju03', 'number', ['number']),
-        /** double eraFasa03(double t); */
-        fasa03: LIBERFA.cwrap('eraFasa03', 'number', ['number']),
-        /** double eraFaur03(double t); */
-        faur03: LIBERFA.cwrap('eraFaur03', 'number', ['number']),
-        /** double eraFane03(double t); */
-        fane03: LIBERFA.cwrap('eraFane03', 'number', ['number']),
 
         //PrecNutPolar
         /** void eraBi00(double *dpsibi, double *depsbi, double *dra); */
@@ -2457,5 +2414,5 @@
         }
     };
 
-    module.exports = Object.assign(erfa, calendar, astrometry, ephemerides);
+    module.exports = Object.assign(erfa, calendar, astrometry, ephemerides, fundamentalArguments);
 })();
